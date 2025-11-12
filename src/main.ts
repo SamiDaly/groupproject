aa130b7e63ba17e1c34a95563de0ff606a78c855
Jessica Näsman <jessicanasman@Jessicas-MacBook-Pro.local>
2025-11-12 14:19:59 +0100

    added type for fetch data

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,23 @@ import "./style.css";
 
 // API KEY: https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda
 
-const f = fetch("https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda");
-f.then((response) => response.json());
-f.then(() => {});
+fetch("https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda")
+  .then((response) => response.json())
+  .then((data: OmdbResponse) => {
+    createHtml(data.Search);
+  });
 
-console.log(f);
+type Movie = {
+  Poster: string;
+  Title: string;
+  isSelected: boolean;
+};
+
+type OmdbResponse = {
+  Search: Movie[];
+  totalResults: string;
+};
+
+const createHtml = (movies: Movie[]) => {
+  const moviesSection = document.getElementById("movies");
+};
