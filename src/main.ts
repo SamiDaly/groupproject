1f014c89cb9bb4a336af498f2720ef35f3c61671
DittGitHubNamn <DinEmail@exempel.com>
2025-11-12 15:53:14 +0100

    changes

--- src/main.ts
+++ src/main.ts
@@ -2,46 +2,34 @@ import "./style.css";
 
 fetch("https://www.omdbapi.com/?s=tt3896198&apikey=768dbcda")
   .then((response) => response.json())
-
-  .then((data) => {
-    console.log(data);
-  });
-
-/**
-  const section = document.getElementById("show");
-
-  const Movie = () => {
-
-  const Movies = innerHtml 
-
-
-  for(let i = 0; i < length.data; i++ {
-    const div = document.createElement("div");
-    const diva = document.createElement("div");
-    const image = document.createElement("img");
-    const p = document.createElement("p");
-
-
-
-
-    div.appendChild(diva);
-    div.appendChild(diva);
-    div.appendChild(image);
-    p.appendChild(image);
-    console.log(Movie);
-
-  })
-
-
-};
- */
-// API KEY: https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda
-
-fetch("https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda")
-  .then((response) => response.json())
   .then((data: OmdbResponse) => {
     createHtml(data.Search);
   });
+
+const Movie = () => {
+  const section = document.getElementById("show");
+  const movie = document.getElementById("movies");
+
+  for (let i = 0; i < Movie.length; i++) {
+    const movie = [i];
+
+    const div = document.createElement("div");
+
+    const img = document.createElement("img");
+
+    const p = document.createElement("p");
+
+    // lägg till elementen i div
+    div.appendChild(p);
+    div.appendChild(img);
+    div.appendChild(p);
+
+    // lägg till div i containern
+    div.appendChild(div);
+  }
+};
+
+// API KEY: https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda
 
 type Movie = {
   Poster: string;
