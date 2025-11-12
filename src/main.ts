68dd792bf279122617fd08e4bce21af939e03231
DittGitHubNamn <DinEmail@exempel.com>
2025-11-12 14:02:51 +0100

    sami

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,22 @@ import "./style.css";
 
 fetch("https://www.omdbapi.com/?s=tt3896198&apikey=768dbcda")
   .then((response) => response.json())
-  .then((data) => {});
+
+  .then((data) => {
+    console.log(data);
+  });
+
+const Movie = () => {
+  const section = document.getElementById("show");
+
+  const div = document.createElement("div");
+  const diva = document.createElement("div");
+  const image = document.createElement("img");
+  const p = document.createElement("p");
+
+  div.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(image);
+  image.appendChild(p);
+  console.log(Movie);
+};
