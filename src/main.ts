5e98c283510c9878ee0f0062cea2b9ac62d47773
Jessica Näsman <jessicanasman@Jessicas-MacBook-Pro.local>
2025-11-12 13:56:54 +0100

    api request

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./style.css";
+
+// API KEY: https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda
+
+const f = fetch("https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda");
+f.then((response) => response.json());
+f.then(() => {});
+
+console.log(f);
