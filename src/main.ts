b82c33363c20562aef48067da8d58bf52463d5ac
Jessica Näsman <jessicanasman@Jessicas-MacBook-Pro.local>
2025-11-12 15:17:48 +0100

    display data in html

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ fetch("https://www.omdbapi.com/?s=tt3896198&apikey=768dbcda")
  */
 // API KEY: https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda
 
-fetch("https://www.omdbapi.com/?i=tt3896198&apikey=768dbcda")
+fetch("http://www.omdbapi.com/?apikey=768dbcda&s=lord")
   .then((response) => response.json())
   .then((data: OmdbResponse) => {
     createHtml(data.Search);
@@ -56,4 +56,26 @@ type OmdbResponse = {
 
 const createHtml = (movies: Movie[]) => {
   const moviesSection = document.getElementById("movies");
+
+  if (moviesSection) {
+    moviesSection.innerHTML = "";
+  }
+
+  movies.forEach((movie, i) => {
+    const movieContainer = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const img = document.createElement("img");
+    const title = document.createElement("h2");
+
+    img.src = movie.Poster;
+    img.alt = movie.Title;
+    title.innerHTML = movie.Title;
+    movieContainer.className = "movie";
+    imgContainer.className = "img-container";
+
+    imgContainer.appendChild(img);
+    movieContainer.appendChild(title);
+    movieContainer.appendChild(imgContainer);
+    moviesSection?.appendChild(movieContainer);
+  });
 };
